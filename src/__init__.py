4d7e4f96aee3dc12d3a2b20759147253574afa05
Ladvien <cthomasbrittain@hotmail.com>
2025-06-16 11:15:37 -0500

    Refactoring.

---
+++ src/__init__.py
@@ -0,0 +1,34 @@
+# chatter_tool/__init__.py
+
+from .server.app import create_app
+from .server.app import TTSRequest, TTSResponse, VoiceInfo, HealthResponse
+from .server.config import TTSServerConfig
+from .tts_services.chatter_box_tts_service import (
+    ChatterboxTTSService,
+    ChatterboxTTSServiceConfig,
+    ChatterboxPipelineConfig,
+    ChatterboxResponseConfig,
+)
+from .tts_services.kokoro_tts_service import (
+    KokoroTTSService,
+    KokoroTTSServiceConfig,
+    KokoroPipelineConfig,
+    KokoroResponseConfig,
+)
+from .common.base_tts_service import TTSService
+
+__all__ = [
+    "create_app",
+    "TTSRequest",
+    "TTSResponse",
+    "VoiceInfo",
+    "HealthResponse",
+    "TTSServerConfig",
+    "ChatterboxTTSService",
+    "KokoroTTSService",
+    "KokoroTTSServiceConfig",
+    "KokoroPipelineConfig",
+    "KokoroResponseConfig",
+    "TTSService",
+    "TTSBaseConfig",
+]
